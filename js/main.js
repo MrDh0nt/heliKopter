caba02e79ec6c0c0e3af0d15433821b565a889fd
MrDh0nt <3v3rtdhont@gmail.com>
2016-11-12 12:23:52 +0100

    added some 'sprites' and made the propellors turn

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var hu = 0;
-var kopterImg = new Image();
-//kopterImg.src = '../css/kopterImg.gif';
-
+var frame = 0;
+var koptrImg = document.getElementById("koptr");
+var scale = 10;
+var koptrCenterX = (koptrImg.width / 2) * scale;
+var koptrCenterY = (koptrImg.height / 2) * scale;
 //function for getting the canvas to resize with a window resize
 function resizeWindow(){
     canvas.width = window.innerWidth*0.8;
@@ -24,18 +26,24 @@ var draw = {
     clearScreen: function(){
         //console.log(hue);
         ctx.beginPath();
-        ctx.fillStyle = "#000";
+        ctx.fillStyle = "#FFF";
         ctx.rect(0, 0, canvas.width, canvas.height);
         ctx.fill();
         ctx.closePath();
     },
     backGroundBody: function(hue){
         document.body.style.backgroundColor = "hsl(" + hue + ", 50%, 42%)";
-    }/*,
-    koptr: function(){
-        ctx.beginPath();
-        ctx.drawImage()
-    }*/
+    },
+    smooth: function(tf){
+        ctx.mozImageSmoothingEnabled = tf;
+        ctx.webkitImageSmoothingEnabled = tf;
+        ctx.msImageSmoothingEnabled = tf;
+        ctx.imageSmoothingEnabled = tf;  
+    },
+    koptr: function(scene, x, y){
+        draw.smooth(false); //drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
+        ctx.drawImage(koptrImg, scene * 32, 0, 32, 16, x, y, 32 * scale, 16 * scale);
+    }
 };
 
 function gameLoop(){
@@ -46,6 +54,11 @@ function gameLoop(){
         hu = 0;
     }
     draw.backGroundBody(hu);
-    //draw.koptr();
+    draw.koptr(frame, 0, 0);
+    if(frame < 15){
+        frame += 1;
+    }else if(frame >= 15){
+        frame = 0;
+    }
 }
 setInterval(gameLoop, 25);
